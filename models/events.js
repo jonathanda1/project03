25a52db5e757f33d103aa877203b9d9e09f14247
Jonathan Dai <jonathanjddai@gmail.com>
2016-05-02 09:11:56 -0700

    updated event schema

--- models/events.js
+++ models/events.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var eventSchema = new mongoose.Schema({
     location: String,
     artist: String,
-    time:
+    time: Date
 })
 
 var Event = mongoose.model('Event', eventSchema);
