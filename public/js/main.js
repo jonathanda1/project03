d3b2c55496260971aa9f3cba1502e9e064cef913
Noah Flick <noahflick@gmail.com>
2016-05-03 17:49:42 -0700

    search-button

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,22 @@
+$('#search-button').click(function(event){
+  console.log('clicked')
+  var name = $('#search-string').val()
+console.log(name)
+  // $.ajax({
+  //   method: 'GET',
+  //   url: '/api/artist/rihanna/events',
+  //   success: function(data){
+  //     var eventlist = data
+  //     console.log(eventlist)
+  //   }
+  // }
 
-console.log("hi main")
+  // );
+
+
+});
+
+
+
+
 
