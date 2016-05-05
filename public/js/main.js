261ac907293d02d1baa62522cc7f911a7183bb21
Noah Flick <noahflick@gmail.com>
2016-05-04 19:09:42 -0700

    update event to db

--- public/js/main.js
+++ public/js/main.js
@@ -16,30 +16,50 @@ console.log(name)
        var event = eventlist.events.event
              $('#searched-event-list').append('<h4>Upcoming events for '+name+'</h4>')
        for(i=0; i<event.length; i++){
-        var searchedEvent = '<div class="eventSample">'+event[i].start_time+' | '+event[i].title+' | '+event[i].venue_name+'<a id='+event[i].id+' class="add-button waves-effect waves-light btn purple accent-2 right">add</a></div><br/>'
+        var searchedEvent = '<div class="eventSample">'+event[i].start_time+' | '+event[i].title+' | '+event[i].venue_name+'<a id=' +i+' class="add-button waves-effect waves-light btn purple accent-2 right">add</a></div><br/>'
 
           $('#searched-event-list').append(searchedEvent)
-
+          $('#'+i).data({
+            venue: event[i].venue_name,
+            title: event[i].title,
+            date: event[i].start_time,
+            imageUrl: event[i].image.medium.url,
+            users: [],
+            eventfulId: event[i].id
+          })
 
        }
        $('.add-button').click(function(e) {
 
-        console.log(e.target.title);
 
-        $.ajax({
-          method: 'PUT',
-          url: '/users/user',
-          data: {
-          eventfulId: e.target.id
-          }
-        }).then(function(user){
-          console.log('ajax worked')
-          console.log(user)
-        })
+        // .then(function(event){
+        //   console.log('ajax worked')
+        //   console.log(event)
+        // })
+        // $.post('/events', {event: event[e.target.id]}, function(data) {
+        //   console.log(data)
+          /*optional stuff to do after success */
+        });
 
-      })
+      // })
+    //    $.ajax({
+    //     method: "POST",
+    //     url: "users/user",
+    //     data: {
+
+    //           },
+    //    })
 
     }
  });
 })
+$(document).on('click', '.add-button', function(){
+  var eventData = $('#'+this.id).data()
+  console.log(eventData)
+  $.post('/events', eventData, function(data) {
+    console.log(data)
+  })
+})
+
+
 
